6d96540f77ec132f5a65fab3b3c13b12b436e406
Pranesh Kumar <codewithpranesh+github@gmail.com>
2025-03-14 18:49:58 +0530

    Forms updated

--- forms.py
+++ forms.py
@@ -21,3 +21,34 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('Username already exists.')
+
+class SubjectForm(FlaskForm):
+    name = StringField('Subject Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    submit = SubmitField('Save Subject')
+
+class ChapterForm(FlaskForm):
+    name = StringField('Chapter Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    subject = SelectField('Subject', coerce=int)
+    submit = SubmitField('Save Chapter')
+
+class QuizForm(FlaskForm):
+    title = StringField('Quiz Title', validators=[DataRequired()])
+    chapter = SelectField('Chapter', coerce=int)
+    duration = IntegerField('Duration (minutes)', validators=[DataRequired()])
+    submit = SubmitField('Save Quiz')
+
+class QuestionForm(FlaskForm):
+    text = TextAreaField('Question Text', validators=[DataRequired()])
+    option1 = StringField('Option 1', validators=[DataRequired()])
+    option2 = StringField('Option 2', validators=[DataRequired()])
+    option3 = StringField('Option 3')
+    option4 = StringField('Option 4')
+    correct_option = SelectField('Correct Option', choices=[
+        (1, 'Option 1'), 
+        (2, 'Option 2'), 
+        (3, 'Option 3'), 
+        (4, 'Option 4')
+    ], coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Save Question')
